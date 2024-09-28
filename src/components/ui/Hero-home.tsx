4f9172033d174d014a90f4f7f6a13b855eb95c3d
Aarohi Bhanuj Chowdhary <146480441+bhanujongit4@users.noreply.github.com>
2024-09-29 04:26:02 +0530

    Update Hero-home.tsx

--- src/components/ui/Hero-home.tsx
+++ src/components/ui/Hero-home.tsx
@@ -99,7 +99,7 @@ export const Herohome = () => {
             <span className="text-lg text-gray-600 dark:text-neutral-400">Dive into the Repository:</span>
             
            
-            <a className="inline-flex items-center gap-x-2 text-lg text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500" href="#">
+            <a className="inline-flex items-center gap-x-2 text-lg text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500" href="https://github.com/bhanujongit4/flaskbackend">
             GitHub Link
               <svg className="shrink-0 size-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
             </a>
